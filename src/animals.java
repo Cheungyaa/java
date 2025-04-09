b6236cfe5ad903170a6b87994afe57d68217ddce
Cheungyaa <netjjang990126@gmail.com>
2025-04-09 14:15:47 +0900

    주석제거

--- src/animals.java
+++ src/animals.java
@@ -21,17 +21,17 @@ class Cat extends Animal {
     }
 }
 
-// 메인 클래스: 실행용
+
 public class animals {
     public static void main(String[] args) {
-        // 부모 타입으로 자식 객체를 다룰 수 있다 → 다형성!
+
         Animal myAnimal = new Animal();
         Animal myDog = new Dog();
         Animal myCat = new Cat();
 
-        // 각각의 sound() 호출 → 오버라이딩된 메서드가 실행됨
-        myAnimal.sound();  // 출력: 동물이 소리를 낸다.
-        myDog.sound();     // 출력: 개가 멍멍한다.
-        myCat.sound();     // 출력: 고양이가 야옹야옹한다.
+
+        myAnimal.sound();
+        myDog.sound();
+        myCat.sound();
     }
 }
